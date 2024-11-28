58cbfbab4cec0440cffa13ccff9fe71cf2439502
Ivy Atieno Okumu <iokumu@safaricom.co.ke>
2024-11-28 20:46:56 +0300

    add a new task and update methods

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-
-import './App.css';
+import React, { useState } from "react";
 import TaskForm from "./components/TaskForm";
+import './App.css';
 
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskToEdit, setTaskToEdit] = useState(null);
+
+  // Add a new task
+  const addTask = (task) => {
+    setTasks([...tasks, { id: Date.now(), ...task }]);
+  };
+
+  // Update an existing task
+  const updateTask = (updatedTask) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === updatedTask.id ? updatedTask : task
+      )
+    );
+    setTaskToEdit(null); // Clear the form
+  };
   return (
     
     <div className="container">
     <div className="tracker">
       <h1 >Task Tracker</h1>
-      <TaskForm/>
+      <TaskForm
+        onAdd={addTask}
+        onUpdate={updateTask}
+        taskToEdit={taskToEdit}
+      />
       </div>
     </div>
   );
 };
 
 export default App;
+
+
+
 
 
 

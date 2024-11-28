8eace9b06816f6d0e09c01fb1ef1d5a2c4f0e7b7
Ivy Atieno Okumu <iokumu@safaricom.co.ke>
2024-11-28 19:55:34 +0300

    Initiat Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Task Tracker
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
